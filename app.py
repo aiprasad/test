a1a0b9f2f57ae10d20aa664ca467d6ef9222264b
aiprasad <49321587+aiprasad@users.noreply.github.com>
2023-05-24 12:24:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from fastapi import FastAPI
+from instruct_pipeline import InstructionTextGenerationPipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 app = FastAPI()
 
-model_name = "databricks/dolly-v2-3b"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-3b", padding_side="left")
+model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-3b", device_map="auto", torch_dtype=torch.bfloat16)
 
-@app.get("/generate")
-def generate_text(prompt: str):
+generate_text = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    data = request.json
+    prompt = data["prompt"]
+    max_length = data.get("max_length", 100)
+
     input_ids = tokenizer.encode(prompt, return_tensors="pt")
-    output = model.generate(input_ids, max_length=100)
+    output = generate_text.generate(input_ids, max_length=max_length)
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-    return {"text": generated_text}
+
+    return jsonify({"text": generated_text})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
